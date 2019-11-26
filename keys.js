58b20e0719abf74020f394ee0fb95774e38421c8
Jose Cuela <jgcuelap@gmail.com>
2019-11-26 13:27:00 -0500

    New Bd

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
 module.exports = {
 
     database: {
+        connectionLimit   :   7,
         host: 'us-cdbr-iron-east-05.cleardb.net',
-        user: 'b303a71bef56f0',
-        password: 'password',
-        database: '5b9ef18e'
+        user: 'b6ffa3b7ffc6ee',
+        password: '71938917',
+        database: 'heroku_74117e62ea86038'
     }
 
 };
