eafd055eeeeaef23cb94d8e1421a426d32d3dd76
Jose Cuela <jgcuelap@gmail.com>
2019-11-30 03:32:03 -0500

    mas pruevas x6

--- routes/index.js
+++ routes/index.js
@@ -652,11 +652,13 @@ router.get('/studio',async (req,res,next) => {
   
   console.log('La fecha de Peru_LIMA ',nDate); */
   let fech = new Date('2019-11-29T23:00:00Z');
-/*   let di = fech.toLocaleString('en-US',{
+
+  let di = fech.toLocaleString('en-US',{
     timeZone: 'Europe/Lisbon'
   });
-  let di2 = new Date(di); */
-  salida = helpers.formatTime(fech)
+
+  let di2 = new Date(di);
+  salida = helpers.formatTime(di2)
   console.log('Salida es ', salida );
 
   res.send(salida);
