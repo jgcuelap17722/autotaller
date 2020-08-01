9574cce10001626040cca2320aec998149e9605c
jose cuela <jgcuelap@gmail.com>
2020-07-31 22:16:03 -0500

    no se hizo cambios

--- keys.js
+++ keys.js
@@ -1,11 +1,11 @@
 module.exports = {
 
-    database: {
-        connectionLimit   :   4,
-        host: 'us-cdbr-iron-east-05.cleardb.net',
-        user: 'b6ffa3b7ffc6ee',
-        password: '71938917',
-        database: 'heroku_74117e62ea86038'
-    }
+  database: {
+    connectionLimit: 4,
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b6ffa3b7ffc6ee',
+    password: '71938917',
+    database: 'heroku_74117e62ea86038'
+  }
 
 };
